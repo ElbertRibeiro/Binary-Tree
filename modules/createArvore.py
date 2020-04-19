e17daf59299b52f1841824d350e11d76302a48cd
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-04-19 12:39:11 -0300

    update arvore

--- modules/createArvore.py
+++ modules/createArvore.py
@@ -4,7 +4,7 @@ class NodoArvore:
         self.esquerda = esquerda
         self.direita = direita
 
-        def __repr__(self):
-            return '%s <- %s -> %s' % ( self.esquerda and self.esquerda.chave,
-                                        self.chave,
-                                        self.direita and self.direita.chave)
+    def __repr__(self):
+        return '%s <- %s -> %s' % ( self.esquerda and self.esquerda.chave,
+                                    self.chave,
+                                    self.direita and self.direita.chave)
