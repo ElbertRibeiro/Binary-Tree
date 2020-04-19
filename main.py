47cf22868ec261f8aabaa9c8d7e9602749a0fbd9
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-04-19 12:58:45 -0300

    update

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import modules.createArvore
 
+'''
+raiz=NodoArvore(40)
+
+raiz.esquerda=NodoArvore(20)
+raiz.direita=NodoArvore(60)
+raiz.direita.esquerda=NodoArvore(50)
+raiz.direita.direita=NodoArvore(70)
+raiz.esquerda.esquerda=NodoArvore(10)
+raiz.esquerda.direita=NodoArvore(30)
+'''
+
 raiz = modules.createArvore.NodoArvore(3)
 raiz.esquerda = modules.createArvore.NodoArvore(5)
 raiz.direita  = modules.createArvore.NodoArvore(1)
