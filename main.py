565e23bd7014dc234ab645c8dc2803e445654391
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-04-19 13:03:45 -0300

    update

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import modules.createArvore
 
-'''
-raiz=NodoArvore(40)
 
-raiz.esquerda=NodoArvore(20)
-raiz.direita=NodoArvore(60)
-raiz.direita.esquerda=NodoArvore(50)
-raiz.direita.direita=NodoArvore(70)
-raiz.esquerda.esquerda=NodoArvore(10)
-raiz.esquerda.direita=NodoArvore(30)
-'''
+raiz = modules.createArvore.NodoArvore(40)
 
-raiz = modules.createArvore.NodoArvore(3)
-raiz.esquerda = modules.createArvore.NodoArvore(5)
-raiz.direita  = modules.createArvore.NodoArvore(1)
+raiz.esquerda = modules.createArvore.NodoArvore(20)
+raiz.direita = modules.createArvore.NodoArvore(60)
+raiz.direita.esquerda = modules.createArvore.NodoArvore(50)
+raiz.direita.direita = modules.createArvore.NodoArvore(70)
+raiz.esquerda.esquerda = modules.createArvore.NodoArvore(10)
+raiz.esquerda.direita = modules.createArvore.NodoArvore(30)
+
+
+#raiz = modules.createArvore.NodoArvore(3)
+#aiz.esquerda = modules.createArvore.NodoArvore(5)
+#raiz.direita  = modules.createArvore.NodoArvore(1)
 print("Árvore: ", raiz)
