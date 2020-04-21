db2fd73011321e2bf523e3d25c79e606154368a5
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-04-21 16:13:29 -0300

    update list tree

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-import modules.createArvore
 import modules.tree
-
-
-'''
-raiz = modules.createArvore.NodoArvore(50)
-
-raiz.esquerda = modules.createArvore.NodoArvore(17)
-raiz.direita = modules.createArvore.NodoArvore(76)
-
-raiz.direita.esquerda = modules.createArvore.NodoArvore(54)
-
-raiz.esquerda.esquerda = modules.createArvore.NodoArvore(9)
-raiz.esquerda.direita = modules.createArvore.NodoArvore(23)
-
-print("Árvore: ", raiz)'''
-
 #listar arvore
 print(modules.tree.funTree())
