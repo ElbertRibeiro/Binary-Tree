c939abe3ef8b2cda051786689955f609448e674e
Elbert Nilton Batista Ribeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2020-04-21 17:00:34 -0300

    up

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import modules.tree
+import modules.searchPreOrdem
 #listar arvore
-print(modules.tree.funTree())
+#print(modules.tree.funTree())
+
+#pre-oredem
+print(modules.searchPreOrdem.funSearch())
+
---
+++ modules/searchPreOrdem.py
@@ -0,0 +1,8 @@
+import modules.tree
+
+search = modules.tree.funTree()
+
+def funSearch():
+   for  search:
+
+  return (search)
